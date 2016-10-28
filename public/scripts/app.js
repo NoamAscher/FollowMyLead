c2069e3429c43efa545845da479d74de17ea6d9b
nicadams <nic.adams@hotmail.com>
2016-10-28 20:45:09 +0000

    Database reconfig and knex connecting

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,20 +1,34 @@
-$(function() {
+// $(function() {
+
+  const knex = require('knex')({
+    client: 'pg',
+    connection: {
+      user     : 'nic',
+      // password : settings.password,
+      database : 'vagrant',
+      host     : 'localhost:3000',
+      // ssl      : settings.ssl,
+      // port     : settings.port
+  }
+});
+
+  knex.select().from('locations').timeout(1000)
 
   // $.ajaxSetup({
   //   url: "/api/"
   // })
 
-  $.ajax({
-    url: 'api/',
-    method: 'GET',
-    dataType: 'json',
-    success: function (result) {
-      $('#tweetContainer').append(renderTweets(result));
-    },
-    error: function() {
+  // $.ajax({
+  //   url: 'api/',
+  //   method: 'GET',
+  //   dataType: 'json',
+  //   success: function (result) {
+  //     $('#tweetContainer').append(renderTweets(result));
+  //   },
+  //   error: function() {
 
-    }
-  })
+  //   }
+  // })
 
 
-});
+// });
--- server/index.js
+++ server/index.js
@@ -5,16 +5,37 @@ const bodyParser = require("body-parser");
 const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 3000;
-// const methodOverride = require('method-override');
+const methodOverride = require('method-override');
 
+// Start your server with this command to make it go to production mode.
+//    NODE_ENV=production node index.js
+const NODE_ENV = process.env.NODE_ENV || "development";
+const knexConnectionInfo = require('../knexfile')[NODE_ENV];
+console.log("Connecting to database...")
+const knex = require('knex')(knexConnectionInfo);
 
-// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/../public"));
-// app.use(methodOverride('_method'));
-console.log('Ran server')
+app.use(methodOverride('_method'));
+// console.log('Ran server')
   // app.get("/", (req, res) => {
     // res.sendFile(path.join(__dirname + "/../index.html"));
 // });
+
+
+app.get('/locations', function(req, res) {
+  knex.select().from('locations')
+  .then(function(locations) {
+    console.log("got locations", locations)
+    res.send("Got data")
+  })
+  .catch(function(error){
+    console.error(error);
+    res.json({error: {message: error}})
+  })
+
+});
+
 
 
 app.listen(PORT, () => {
