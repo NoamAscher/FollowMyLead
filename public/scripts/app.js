9dc0df64f7fcb1101101f092b4b6e7a637731657
nicadams <nic.adams@hotmail.com>
2016-10-29 19:54:49 +0000

    added fake login/logout

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,23 +1,21 @@
-$(function() {
+// $(function() {
+
+// // edit
+// // delete
+// // new
 
 
+//   $.ajax({
+//     url: '/locations/:id'',
+//     method: 'GET',
+//     dataType: 'json',
+//     success: function (result) {
+//       $('#tweetContainer').append(renderTweets(result));
+//     },
+//     error: function() {
+
+//     }
+//   })
 
 
-  // $.ajaxSetup({
-  //   url: "/api/"
-  // })
-
-  // $.ajax({
-  //   url: 'api/',
-  //   method: 'GET',
-  //   dataType: 'json',
-  //   success: function (result) {
-  //     $('#tweetContainer').append(renderTweets(result));
-  //   },
-  //   error: function() {
-
-  //   }
-  // })
-
-
-});
+// });
