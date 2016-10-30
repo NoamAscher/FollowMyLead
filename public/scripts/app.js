e69d42fd37fe771d8b0e6d4aef9390824d2b84f9
Noam <noam.ascher@gmail.com>
2016-10-30 23:39:42 +0000

    other-user maps loading

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,68 +1,3 @@
-
-
-/* AJAX needs
-
-- on DOM ready, how to run the following routes:
-  - load user info
-  - load followers (favourite maps would load on an event)
-  - load the map of that user
-  - people following the map
-
-- loadFollowing function - dynamically alter the page accordingly (the purple version)
-
-*/
-
-
-// map loading stuff (ported from html file)
-
-// let tokenURL = 'https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoicG90YXRvd2F2ZSIsImEiOiJjaXVzbzlsbHIwMGZhMnVwdmVoMGphOHNvIn0.HyG4kMGYnE6zVYU6IBr66Q';
-// let attribution = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>';
-// var map = L.map('initialmap').setView([49.2566, -123.11554], 5);
-// // here is a pretend set of locations.
-// $.get( "api/locations/2", function(data) {
-//   const locationsArray = [];
-//   let newLocation;
-//   function onMapClick(e) {
-//     if (newLocation !== undefined) {
-//       map.removeLayer(newLocation);
-//     }
-//     let coords = [e.latlng.lat, e.latlng.lng];
-//     newLocation = L.marker(coords).bindPopup("i am a form" + coords).addTo(map).openPopup();
-//   }
-//   map.on('click', onMapClick);
-//   for (let i = 0; i < data.length; i++) {
-//     //let handle = data[i].handle;
-//     let avatar = data[i].avatar;
-//     let summary = data[i].summary;
-//     let name = data[i].name;
-//     let url = data[i].url;
-//     let image = data[i].img;
-//     let category = data[i].category;
-//     let html = renderHTML("handle", image, summary, url, name);
-//     let coords = [data[i].latitude, data[i].longitude];
-//     locationsArray.push(L.marker(coords).bindPopup(html));
-//     L.layerGroup(locationsArray).addTo(map);
-//   };
-// });
-// L.tileLayer(tokenURL, { attribution: attribution, maxZoom: 18})
-// .addTo(map);
-// // the following method takes in an array of points to add to the map view
-// function renderHTML(handle, avatar, description, url, place) {
-//   var $section = $("<section>").addClass("popup-container").html('<img src=\'' + avatar + '\' class="popup-image"><br>' + '<span class="popup-placetitle"><h6>' + place + '</h6></span>');
-//   var $header = $("<header>").addClass("popup-header");
-//   var $content = $("<content>").addClass("popup-body").html(description + '<p>' + '<a class="popup-link" href=' + url + '>' + url + '</a>');
-//   var $footer = $("<footer>").addClass("popup-footer").html("<button type='button' class='popup-edit'>Edit</button><button type='button' class='popup-delete'>Delete</button>");
-//   $section.append($header);
-//   $section.append($content);
-//   $section.append($footer);
-//   return $($section).prop('outerHTML');
-// };
-
-
-// subfunctions for the sidebar:
-
-
-
 var userInfoInSidebar = function(users) {
   return `
     <article class="user">
@@ -85,7 +20,7 @@ var favouriteMapInSidebar = function(favouriteMapInfo) {
         <img class="avatar" src="${favouriteMapInfo.avatar}">
       </div>
       <div class="right">
-        <div class="handle">${favouriteMapInfo.handle}:</div>
+        <div class="handle">${favouriteMapInfo.handle}'s map:</div>
         <div class="map-name">"${favouriteMapInfo.name}"</div>
         <div class="map-id" style="display: none">"${favouriteMapInfo.map_id}"</div>
       </div>
@@ -271,9 +206,11 @@ $(document).ready(function() {
   });
 
   $('.favorites-body').on('click', 'img', function(event) {
+    $('#initialmap').empty();
     let tokenURL = 'https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoicG90YXRvd2F2ZSIsImEiOiJjaXVzbzlsbHIwMGZhMnVwdmVoMGphOHNvIn0.HyG4kMGYnE6zVYU6IBr66Q';
     let attribution = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>';
-    var map = markers.clearLayers();
+    map.remove();
+    map = L.map('initialmap').setView([49.2566, -123.11554], 11);
 
     var mapId = $(this).parent().next().find('.map-id').html();
     mapId = mapId.substring(1,2);
@@ -327,29 +264,4 @@ $(document).ready(function() {
 
   });
 
-}); // temp close bracket
- // and another
-
-
-
-
-
-
-//   $.ajaxSetup({
-//     url: "/api/"
-//   })
-
-//   $.ajax({
-//     url: 'api/',
-//     method: 'GET',
-//     dataType: 'json',
-//     success: function (result) {
-//       $('#tweetContainer').append(renderTweets(result));
-//     },
-//     error: function() {
-
-//     }
-//   })
-
-
-// });
+});
